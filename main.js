76d584b7cdd5eea8ad933524501822b90c534772
Kim HyunWoo <stories2@naver.com>
2021-05-06 00:36:06 +0900

    letters, gradient, stroke

--- main.js
+++ main.js
@@ -12,6 +12,16 @@ window.onload = () => {
 
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
+        const gradient1 = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+        gradient1.addColorStop(0.2, 'pink');
+        gradient1.addColorStop(0.3, 'red');
+        gradient1.addColorStop(0.8, 'orange');
+        gradient1.addColorStop(0.4, 'yellow');
+        gradient1.addColorStop(0.5, 'green');
+        gradient1.addColorStop(0.6, 'turquoise');
+        gradient1.addColorStop(0.7, 'violet');
+
+        const letters = ['H', 'E', 'S', 'T', 'I', 'A'];
 
         ctx.drawImage(image, (canvas.width - Math.min(canvas.width, canvas.height) * imgRatio) / 2, 0, Math.min(canvas.width, canvas.height) * imgRatio, Math.min(canvas.width, canvas.height));
         const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
@@ -30,6 +40,7 @@ window.onload = () => {
                 const brightness = calcRelativeBrightness(red, green, blue);
                 const cell = [
                     cellBrightness = brightness,
+                    cellColor = `rgb(${red},${green},${blue})`
                 ]
                 row.push(cell);
             }
@@ -51,27 +62,55 @@ window.onload = () => {
                 this.size = Math.random() * 1.5 + 1;
                 this.pos1 = Math.floor(this.y);
                 this.pos2 = Math.floor(this.x);
+
+                this.angle = 0;
+
+                this.letter = letters[Math.floor(Math.random() * letters.length)];
+                this.random = Math.random();
             }
 
             update() {
                 this.pos1 = Math.floor(this.y);
                 this.pos2 = Math.floor(this.x);
-                this.speed = mappedImage[this.pos1][this.pos2][0];
+                
+                if (mappedImage[this.pos1] && mappedImage[this.pos1][this.pos2]) {
+                    this.speed = mappedImage[this.pos1][this.pos2][0];
+                }
                 let movement = (2.5 - this.speed) + this.velocity;
+                this.angle += this.speed / 20;
+                this.size = this.speed;
 
-                this.y += movement;
+                this.y += movement + Math.sin(this.angle) * 2;
+                this.x += movement + Math.cos(this.angle) * 2;
 
                 if (this.y >= canvas.height) {
                     this.y = 0;
                     this.x = Math.random() * canvas.width;
 
                 }
+                if (this.x >= canvas.width) {
+                    this.x = 0;
+                    this.y = Math.random() * canvas.height;
+                }
             }
 
             draw() {
                 ctx.beginPath();
-                ctx.fillStyle = 'white';
-                ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+                if (mappedImage[this.pos1] && mappedImage[this.pos1][this.pos2]) {
+                    ctx.fillStyle = mappedImage[this.pos1][this.pos2][1];
+                    ctx.strokeStyle = mappedImage[this.pos1][this.pos2][1];
+                }
+                // ctx.fillStyle = gradient1;
+                // ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+                // ctx.strokeRect(this.x, this.y, this.size * 2, this.size * 2);
+                if (this.random < 0.3) {
+                    ctx.font = '10px Arial';
+                    ctx.fillText(this.letter, this.x, this.y);
+                } else if (0.3 <= this.random && this.random < 0.6) {
+                    ctx.strokeRect(this.x, this.y, this.size * 2, this.size * 2);
+                } else {
+                    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+                }
                 ctx.fill();
             }
         }
@@ -91,7 +130,8 @@ window.onload = () => {
             ctx.globalAlpha = 0.25;
             particleArr.forEach(particle => {
                 particle.update();
-                ctx.globalAlpha = particle.speed * 0.25;
+                // ctx.globalAlpha = particle.speed * 0.25;
+                ctx.globalAlpha = 1;
                 particle.draw();
             });
 
